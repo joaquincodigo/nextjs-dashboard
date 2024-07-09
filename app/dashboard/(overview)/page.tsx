69ac77836eaf0f944610f6eb2e14baf7c636f1cc
joaquincodigo <joaquin.codigo@gmail.com>
2024-07-09 18:15:26 -0300

    dynamic partial rendering of LatestInvoices with skeleton

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,6 +6,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices } from '@/app/lib/data'
 import { fetchCardData } from '@/app/lib/data'
 import {RevenueChartSkeleton} from '@/app/ui/skeletons'
+import {LatestInvoicesSkeleton} from '@/app/ui/skeletons'
 
 export default async function Page() {
 
@@ -33,10 +34,15 @@ export default async function Page() {
         />}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+
         <Suspense fallback={<RevenueChartSkeleton/>}>
          <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+
+        <Suspense fallback={<LatestInvoicesSkeleton/>}>
+          <LatestInvoices />
+        </Suspense>
+
       </div>
     </main>
   );
